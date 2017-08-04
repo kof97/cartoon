223491b03f7dafc27706d2f88330b1e037799717
Arno <1048434786@qq.com>
2017-08-04 10:23:38 +0800

    source

--- spider.php
+++ spider.php
@@ -2,7 +2,9 @@
 
 require 'src/loader.php';
 
-$json = $_GET['json'];
+$url = 'http://hhzapi.ishuhui.com/cartoon/post/ver/76906890/id/9055.json';
+
+$json = file_get_contents($url);
 
 $uri = array(
 	'http://pic01.ishuhui.com/',
@@ -21,8 +23,10 @@ $img_list = json_decode($json['data']['content_img'], true);
 foreach ($img_list as $num => $value) {
 	$img_url = $uri[mt_rand(0, count($uri) - 1)] . substr($value, 8);
 
+	$img = file_get_contents($img_url);
 
-	// file_put_contents('./aaa.png', $img);
+
+	file_put_contents('./aaa.png', $img);die;
 }
 
 // end of script
